d6105d04a90300c5396fd945cf53569672d3b3eb
Patel7Manav <88702825+Patel7Manav@users.noreply.github.com>
2022-03-10 13:13:17 +0530

    Time: 56 ms (69.40%), Space: 35.1 MB (6.37%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int issafe(vector<int>a,int h,int mi)
+    {
+        int c=0;
+        int n=a.size();
+        for(int i=0;i<n;i++)
+        {
+            c+=ceil(double(a[i])/mi);
+            if(c>h)
+                return 0;
+        }
+        return 1;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(),piles.end());
+        int n=piles.size();
+        if(h==n)
+            return piles[n-1];
+        int lo=1,hi=piles[n-1];
+        int ans=0;
+        while(lo<=hi)
+        {
+            int mi=lo+(hi-lo)/2;
+            cout<<mi<<" ";
+            if(issafe(piles,h,mi))
+            {
+                ans=mi;
+                hi=mi-1;
+            }
+            else
+                lo=mi+1;
+        }
+        return ans;
+    }
+};
