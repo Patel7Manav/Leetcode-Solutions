9e9614b837c8f7cf1f17f57fef173e055d437ea2
Patel7Manav <88702825+Patel7Manav@users.noreply.github.com>
2022-05-10 13:55:51 +0530

    Time: 43 ms (22.30%), Space: 155.8 MB (7.21%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int solve(int n,int cop,int curr)
+    int solve(int n,int cop,int curr,vector<vector<int>>&dp)
     {
         if(curr>n)
             return 1e8;
         if(curr==n)
             return 0;
+        if(dp[cop][curr]!=0)
+            return dp[cop][curr];
         int left=1e8;
         if(cop!=0)
-         left=1+solve(n,cop,curr+cop);  
-        int right=2+solve(n,curr,curr+curr);
-        return min(left,right);
+         left=1+solve(n,cop,curr+cop,dp);  
+        int right=2+solve(n,curr,curr+curr,dp);
+        return dp[cop][curr]=min(left,right);
     }
     int minSteps(int n) {
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         if(n==1)
             return 0;
         if(n==2)
             return 2;
-       return solve(n,0,1);
+       return solve(n,0,1,dp);
     }
 };
