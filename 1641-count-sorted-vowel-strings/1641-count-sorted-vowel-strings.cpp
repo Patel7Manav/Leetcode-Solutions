0ab2bf5510f4523d70acec1cf4d06f85d4a4ee77
Patel7Manav <88702825+Patel7Manav@users.noreply.github.com>
2022-05-12 01:31:44 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (22.41%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(vector<char>&arr,int n,string curr,int ind,int len,vector<vector<int>>&dp)
+    int solve(vector<char>&arr,int n,int ind,int len,vector<vector<int>>&dp)
     {
         if(len==n)
         {  
@@ -11,7 +11,7 @@ public:
         int ans=0;
         for(int i=ind;i<5;i++)
         {
-           ans+= solve(arr,n,curr+arr[i],i,len+1,dp);
+           ans+= solve(arr,n,i,len+1,dp);
         }
         return dp[ind][len]=ans;
     }
@@ -24,11 +24,11 @@ public:
         arr.push_back('o');
         arr.push_back('u');
         int ans=0;
-       ans+= solve(arr,n,"a",0,1,dp);
-       ans+= solve(arr,n,"e",1,1,dp);
-       ans+= solve(arr,n,"i",2,1,dp);
-       ans+= solve(arr,n,"o",3,1,dp);
-       ans+= solve(arr,n,"u",4,1,dp);
+       ans+= solve(arr,n,0,1,dp);
+       ans+= solve(arr,n,1,1,dp);
+       ans+= solve(arr,n,2,1,dp);
+       ans+= solve(arr,n,3,1,dp);
+       ans+= solve(arr,n,4,1,dp);
         return ans;
     }
 };
