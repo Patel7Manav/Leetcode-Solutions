af79e758a72c0328e4ccd2004045092ea00c92d0
Patel7Manav <88702825+Patel7Manav@users.noreply.github.com>
2022-11-28 17:17:01 +0530

    Time: 198 ms (96.44%), Space: 71.5 MB (23.03%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& pr, vector<vector<int>>& queries) {
+        if(pr.size()==0)
+        {
+            vector<bool>sol(queries.size(),false);
+            return sol;
+        }
+        vector<int> adj[numCourses];
+        for(int i=0;i<pr.size();i++)
+        {
+            adj[pr[i][1]].push_back(pr[i][0]);
+        }
+        vector<set<int>>rel(numCourses);
+        
+       for(int i=0;i<numCourses;i++)
+       {  
+           queue<int>q;
+        vector<int>vis(numCourses,0);
+        vis[i]=1;
+           for(auto x:adj[i])
+           {
+               q.push(x);
+               vis[x]=1;
+           }
+           while(!q.empty())
+           {
+               auto temp=q.front();
+               q.pop();
+               rel[i].insert(temp);
+               for(auto y:adj[temp])
+               {    if(!vis[y])
+               {q.push(y);
+               vis[y]=1;
+               }
+               }
+           }
+       } 
+        int si=queries.size();
+        vector<bool>sol(si);
+        for(int i=0;i<queries.size();i++)
+        {
+            int u=queries[i][0],v=queries[i][1],lvlu,lvlv;
+            if(rel[v].find(u)!=rel[v].end())
+                sol[i]=true;
+        }
+        return sol;
+    }
+};
