f4a8ed203d1e612cda318ae68f81864cd09a7623
Patel7Manav <88702825+Patel7Manav@users.noreply.github.com>
2022-11-29 18:27:10 +0530

    Time: 808 ms (48.43%), Space: 58 MB (47.97%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countPairs(vector<int>& del) {
+     unordered_map<int,int>m;
+        vector<int>pow2;
+        pow2.push_back(0);
+        for(int i=0;i<=21;i++)
+            pow2.push_back(pow(2,i));
+       long int ans=0;
+        for(int x:del)
+        {   
+            for(int i=1;i<24;i++)
+            {
+                int diff=pow2[i]-x;
+                if(m.find(diff)!=m.end())
+                {
+                   ans=ans+(m[diff])%1000000007;
+                }
+               
+            }
+             m[x]++;
+        }
+        return ans%int(1000000007);
+    }
+};
