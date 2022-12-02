66e85c9863b410dffe943b319c4c5675e42ec523
Patel7Manav <88702825+Patel7Manav@users.noreply.github.com>
2022-12-02 16:41:43 +0530

    Time: 81 ms (72.17%), Space: 16 MB (52.51%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>&mat,vector<vector<int>>&vis,int val,vector<vector<int>>&dp)
+    int solve(int i,int j,vector<vector<int>>&mat,int val,vector<vector<int>>&dp)
     {
-        if(i>=mat.size() || i<0 || j>=mat[0].size() || j<0 || vis[i][j] || mat[i][j]<=val)
+        if(i>=mat.size() || i<0 || j>=mat[0].size() || j<0 ||  mat[i][j]<=val)
             return 0;
           
             if(dp[i][j]!=-1)
@@ -11,24 +11,23 @@ public:
             }
         
         int left=0,right=0,top=0,down=0;
-        vis[i][j]=1;
-        left=solve(i-1,j,mat,vis,mat[i][j],dp);
-        right=solve(i+1,j,mat,vis,mat[i][j],dp);
-        down=solve(i,j+1,mat,vis,mat[i][j],dp);
-        top=solve(i,j-1,mat,vis,mat[i][j],dp);
-        vis[i][j]=0;
+       
+        left=solve(i-1,j,mat,mat[i][j],dp);
+        right=solve(i+1,j,mat,mat[i][j],dp);
+        down=solve(i,j+1,mat,mat[i][j],dp);
+        top=solve(i,j-1,mat,mat[i][j],dp);
+        
         return dp[i][j]=1+max(max(max(left,right),top),down);
     
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
      int m=matrix.size(),n=matrix[0].size(),ans=0;
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {   
-                ans=max(ans,solve(i,j,matrix,vis,-1,dp));
+                ans=max(ans,solve(i,j,matrix,-1,dp));
             }
         }
         return ans;
